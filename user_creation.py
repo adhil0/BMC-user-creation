8bf6c15f61dd04b178f35c991bfcf8f144276540
Adhitya Logan <alogan@redhat.com>
2022-12-02 16:25:16 -0800

    add initial iteration of script that just reads in yml

---
+++ user_creation.py
@@ -0,0 +1,31 @@
+#TODO: take in yaml of IP's and creds, and new user/pass
+#TODO: check if all creds present
+#TODO: go thru existing accounts and check if username glpi exists at redfish/v1/AccountService/Accounts/ ['UserName']
+#TODO: if not, create one
+import urllib3
+import argparse
+import yaml
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def main() -> None:
+    """Main function"""
+    # Get the command line arguments from the user.
+    parser = argparse.ArgumentParser(
+        description="GLPI Computer REST reservation check."
+    )
+    parser.add_argument(
+        "-i",
+        "--info",
+        metavar="info",
+        help="path to credential YAML file",
+    )
+    args = parser.parse_args()
+    info_path = args.info
+    with open(info_path, "r"):
+        info_dict = yaml.safe_load(info_path)
+    print(info_dict)
+
+# Executes main if run as a script.
+if __name__ == "__main__":
+    main()
